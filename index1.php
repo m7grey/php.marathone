821ee3e7e620b346a43aa2c3e87a3884a479e0f0
m7grey <m7grey@gmail.com>
2017-03-17 11:35:02 -0400

    Final plus real experience.

--- index1.php
+++ index1.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: serhiyyarosh
- * Date: 3/4/17
- * Time: 1:25 AM
- */
+if (count($_POST) > 0) {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+
+    file_put_contents('apps.txt', "$name $phone\n", FILE_APPEND);
+
+    echo 'Your request accepted, please wait for our call!';
+}
+?>
+
+<form method="post">
+    Name<br>
+    <input type="text" name="name" required><br>
+    Mobile<br>
+    <input type="text" name="phone"><br>
+    <input type="submit" value="Send">
+
+</form>
+
